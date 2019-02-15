6fd6b6bd8ff7e31b4e85e46b9dbc8fba716763f9
Alfredo V <alfredo.villagomez.m@gmail.com>
2019-02-15 11:00:42 -0400

    Book Card style  Added

---
+++ Books App/Application/Main/BooksViewController.swift
@@ -0,0 +1,43 @@
+//
+//  BooksViewController.swift
+//  Books App
+//
+//  Created by Alfredo Villagomez on 2/15/19.
+//  Copyright © 2019 Alfredo Villagomez. All rights reserved.
+//
+
+import UIKit
+
+class BooksViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension BooksViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+    }
+    
+    
+}
